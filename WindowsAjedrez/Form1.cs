b93bf4e49fe65e2cd88bf009aac15f97b48da2b2
Juanmaponce <65909673+Juanmaponce@users.noreply.github.com>
2022-11-01 16:37:05 -0300

    Add project files.

---
+++ WindowsAjedrez/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using LibAjedrez;
+namespace WindowsAjedrez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPiezas_Click(object sender, EventArgs e)
+        {
+            Caballo caballo = new Caballo();
+            Peon peon = new Peon();
+            Torre torre = new Torre();
+
+            MessageBox.Show(caballo.Mover());
+            MessageBox.Show(torre.Mover());
+            MessageBox.Show(peon.Mover());
+
+
+        }
+    }
+}
